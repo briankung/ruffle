1394ab69296d0cfa7555859ff1bfeae996813c47
Brian Kung <github@briankung.xyz>
2022-01-31 11:30:19 -0600

    Initial commit

---
+++ lib/ruffle.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruffle/version"
+
+module Ruffle
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ruffle_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ruffle do
+  it "has a version number" do
+    expect(Ruffle::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
